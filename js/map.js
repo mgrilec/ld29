d31b44a40f00746e2c919bff32dee0ec78ae9adc
mgrilec <mihael@mgrilec.com>
2014-04-26 16:40:16 +0200

    Started map.

--- js/map.js
+++ js/map.js
@@ -1,5 +1,23 @@
 var map = {
+    
+    levels: {
+        staringHeight: -50,
+        height: 100,
+        
+        onEnter: function(i) {};
+    },
+    
+    preload: function() {
+    
+    },
+    
     create: function() {
         
-    }
+    },
+    
+    update: function() {
+    },
+    
+    render: function() {
+    },
 }
